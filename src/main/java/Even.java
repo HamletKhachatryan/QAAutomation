ae004cb215a52828d02d0bb41598eee55c07a8d6
Asus <khachatrynah1985@gmail.com>
2022-02-18 19:23:18 +0400

    Homework3

---
+++ src/main/java/Even.java
@@ -0,0 +1,14 @@
+public class Even {
+
+        public static void main(String args[]) {
+            int x = 0;
+            System.out.print("Even numbers from zero to one hundred\n");
+            do {
+                x += 2;
+                System.out.print("x= " + x);
+                System.out.print("\n");
+            } while (x < 100);
+
+
+        }
+}
